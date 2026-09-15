d97d8354e59eb4b7c5c8292aacf35d6e93bf0881
nicetrykiddo <bestxd916@gmail.com>
2026-09-15 12:09:42 +0530

    feat: add Division class and update Main class structure

--- Main.java
+++ Main.java
@@ -1,32 +1,5 @@
 import java.util.*;
-
-class Calculator{
-    int a;
-    int b;
-    String op;
-    double ans;
-
-    public void parse(int a, String op, int b){
-        this.a = a;
-        this.b = b;
-        this.op = op;
-        switch (op){
-            case "+":
-                this.ans = add(this.a, this.b);
-                display(a,op,b,ans);
-                break;
-        }
-    }
-
-    private double add(int a, int b){
-        return (double)(a+b);
-    }
-
-    public void display(int a, String op, int b, double ans){
-        System.out.println(a + " " + op + " " + b + " = " + ans);
-    }
-
-}
+import 
 
 public class Main{
     public static void main(String[] args){
