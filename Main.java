cef07423f622b1caa211111824bfb2be5adba123
nicetrykiddo <bestxd916@gmail.com>
2026-09-15 12:12:04 +0530

    oops

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 import java.util.*;
-import 
 
 public class Main{
     public static void main(String[] args){
         System.out.println("This is a calculator Application.");
-        Calculator calc = new Calculator();
-        calc.parse(5, "+", 6);
     }
 }
